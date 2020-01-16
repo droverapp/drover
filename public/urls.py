28cdc5229304a0a998ceda636cd9c28219062af2
Saptak S <saptak013@gmail.com>
2020-01-16 12:12:11 +0530

    Sends automated SMS if replied to the SMS

--- public/urls.py
+++ public/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('mapview/', views.mapview, name='mapview'),
+    path('sms-reply/', views.sms_reply, name='sms_reply'),
 ]
--- public/views.py
+++ public/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from twilio.twiml.messaging_response import Message, MessagingResponse
 
 def home(request):
     return render(request, 'home.html')
@@ -7,4 +10,17 @@ def mapview(request):
     if request.GET.get('lon', None) and request.GET.get('lat', None):
         return render(request, 'mapview.html', 
         {'lat': request.GET.get('lat', None), 'lon': request.GET.get('lon', None)})
-    return render(request, 'home.html')
+    return redirect('home')
+
+@csrf_exempt
+def sms_reply(request):
+    if request.method == 'POST':
+        """Respond to incoming calls with a simple text message."""
+        # Start our TwiML response
+        resp = MessagingResponse()
+
+        # Add a message
+        message = resp.message("Do Not Reply via SMS. Please visit the link given in previous message to send a reply")
+
+        return HttpResponse(str(message))
+    return redirect('home')
